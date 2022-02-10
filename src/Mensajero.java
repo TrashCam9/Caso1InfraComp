e7e6d3a4bf373eb10e01a86d5de519c713a76c3b
TrashCam9 <62612964+TrashCam9@users.noreply.github.com>
2022-02-10 13:40:31 -0500

    Add attributes to Mensajero
    Se agrega los estilos de envío y recibido del mensajero, la identificación, el buzón de entrada y de salida y el tiempo que debe esperar para enviar el mensaje.

--- src/Mensajero.java
+++ src/Mensajero.java
@@ -1,3 +1,9 @@
 public class Mensajero extends Thread {
-    
+    private boolean estiloEnvio;
+    private boolean estiloRecibido;
+    private int id;
+    private Buzon buzonEntrada;
+    private Buzon buzonSalida;
+    private int tiempoTransformacion;
+
 }
