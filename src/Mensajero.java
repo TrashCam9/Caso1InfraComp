4d30d62112ebb862f28758a869fa2179a286ed2a
TrashCam9 <62612964+TrashCam9@users.noreply.github.com>
2022-02-16 08:55:45 -0500

    Constructor for Mensajero and documentation

--- src/Mensajero.java
+++ src/Mensajero.java
@@ -1,4 +1,5 @@
 public class Mensajero extends Thread {
+
     private boolean estiloEnvio;
     private boolean estiloRecibido;
     private int id;
@@ -6,4 +7,22 @@ public class Mensajero extends Thread {
     private Buzon buzonSalida;
     private int tiempoTransformacion;
 
+    /**
+     * Constructor para la clase Mensajero
+     * @param estiloEnvio true si el envio es activo, false si el envio es pasivo
+     * @param estiloRecibido true si el recibido es activo, false si el envio es pasivo
+     * @param id Numero entre el 1 y el 4 que describe la identificación del mensajero
+     * @param buzonEntrada Referencia al buzon que tendrá los mensajes que va a recibir
+     * @param buzonSalida Referencia al buzon que tendrá los mensajes que va a enviar
+     * @param tiempoTransformacion Tiempo en milisegundos de demora de envío del mensajero
+     */
+    public Mensajero(boolean estiloEnvio, boolean estiloRecibido, int id, Buzon buzonEntrada, Buzon buzonSalida,
+            int tiempoTransformacion) {
+        this.estiloEnvio = estiloEnvio;
+        this.estiloRecibido = estiloRecibido;
+        this.id = id;
+        this.buzonEntrada = buzonEntrada;
+        this.buzonSalida = buzonSalida;
+        this.tiempoTransformacion = tiempoTransformacion;
+    }
 }
