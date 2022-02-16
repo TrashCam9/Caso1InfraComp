505a511d7a35e135704c7d2f782eb788f003f86f
TrashCam9 <62612964+TrashCam9@users.noreply.github.com>
2022-02-16 16:34:04 -0500

    Documentation for Buzon' constructor

--- src/Buzon.java
+++ src/Buzon.java
@@ -4,6 +4,10 @@ public class Buzon {
     private ArrayList<String> mensajes;
     private int size;
 
+    /**
+     * Constructor de Buzon
+     * @param size Tamaño del buzon
+     */
     public Buzon(int size){
         this.size = size;
         this.mensajes = new ArrayList<String>(size);
